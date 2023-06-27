36f4a9245f98ae7f3025ccc9a4232938c5819311
leanwang <leanwang@zego.im>
2023-06-27 15:17:52 +0800

    feat: add mdx test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import "../styles/editor.scss";
+import "tailwindcss/tailwind.css"; // 引入 Tailwind 的默认样式
+import "../tailwind.config.js"; // 引入自定义配置
+import "prismjs/themes/prism-okaidia.min.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/docs/[[...slug]].tsx
+++ pages/docs/[[...slug]].tsx
@@ -1,6 +1,6 @@
-import { getMDXComponent } from 'mdx-bundler/client';
-import { useMemo } from 'react';
-import { getAllDocsInfo, getDocData } from '../../lib/docs';
+import { getMDXComponent } from "mdx-bundler/client";
+import { useMemo } from "react";
+import { getAllDocsInfo, getDocData } from "../../lib/docs";
 
 export const getStaticProps = async ({ params }) => {
   const postData = await getDocData(params.slug);
@@ -25,13 +25,13 @@ export default function DocPage({ code, frontmatter }) {
   const Component = useMemo(() => getMDXComponent(code), [code]);
 
   return (
-    <>
+    <div className="prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none">
       <h1>{frontmatter.title}</h1>
       <p>{frontmatter.description}</p>
       <p>{frontmatter.date}</p>
       <article>
         <Component />
       </article>
-    </>
+    </div>
   );
 }
