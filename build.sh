e325fd06e259730d62b888fea88c50e44e82fb3b
eliaswang <eliaswang@zego.im>
2023-07-18 15:19:24 +0800

    .

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 Rebuild="Rebuild"
+echo "BuildType: $BuildType"
 if [ $BuildType == "$Rebuild" ]
 then # 全量
+    echo "Rebuild"
     python rebuild.py -w $Workspace -s $Site -b $BaseDomain -t $Token
 else # 增量
-    python incr_update.py  -w $Workspace -s $Site -b $BaseDomain -t $Token -p $Project -f $UpdateFiles -o $OutputFolder
+    echo "Incremental"
+    python incr_update.py  -w $Workspace -s $Site -b $BaseDomain -t $Token -p $Project -f "$UpdateFiles" -o "$OutputFolder"
 fi
