3a082e2b0d1d47c8ff0dfd493602f8cfd9b02e66
eliaswang <eliaswang@zego.im>
2023-07-25 12:00:19 +0800

    install pnpm in shell

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ Rebuild="Rebuild"
 if [ "$BuildType" = "$Rebuild" ]
 then # 全量
     echo "Rebuild"
+    sudo npm install -g pnpm
     python rebuild.py -w $Workspace -s $Site -b $BaseDomain -t $Token -c $Callback
 else # 增量
     echo "Incremental"
