cef82b5ae44662c94e27d68528b926b2aaa7c4bf
杨万青 <yangwanqing.me@qq.com>
2023-07-25 11:32:48 +0800

    Support TOC

--- components/preview-layout.tsx
+++ components/preview-layout.tsx
@@ -3,8 +3,10 @@ import { Tree, Select, Space } from 'antd';
 import Router from "next/router";
 import { DownOutlined } from '@ant-design/icons';
 import type { TreeProps } from 'antd/es/tree';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Menu, Anchor } from 'antd';
 // import useSWR from 'swr';
+
+const { Link } = Anchor;
 
 const LANGUAGES = { "af_za": "Afrikaans", "am_et": "አማርኛ", "ar_ar": "العربية", "as_in": "অসমীয়া", "az_az": "Azərbaycanca", "be_by": "Беларуская", "bg_bg": "Български", "bn_in": "বাংলা", "bs_ba": "Bosanski", "ca_es": "Català", "cs_cz": "Čeština", "cy_gb": "Cymraeg", "da_dk": "Dansk", "de_de": "Deutsch", "el_gr": "Ελληνικά", "en_au": "English (Australia)", "en_ca": "English (Canada)", "en_gb": "English (United Kingdom)", "en_in": "English (India)", "en_sg": "English (Singapore)", "en_us": "English (United States)", "en_za": "English (South Africa)", "eo_eo": "Esperanto", "es_ar": "Español (Argentina)", "es_bo": "Español (Bolivia)", "es_cl": "Español (Chile)", "es_co": "Español (Colombia)", "es_cr": "Español (Costa Rica)", "es_do": "Español (República Dominicana)", "es_ec": "Español (Ecuador)", "es_es": "Español (España)", "es_gt": "Español (Guatemala)", "es_hn": "Español (Honduras)", "es_mx": "Español (México)", "es_ni": "Español (Nicaragua)", "es_pa": "Español (Panamá)", "es_pe": "Español (Perú)", "es_pr": "Español (Puerto Rico)", "es_py": "Español (Paraguay)", "es_sv": "Español (El Salvador)", "es_us": "Español (Estados Unidos)", "es_uy": "Español (Uruguay)", "es_ve": "Español (Venezuela)", "et_ee": "Eesti", "eu_es": "Euskara", "fa_ir": "فارسی", "fi_fi": "Suomi", "fil_ph": "Filipino", "fo_fo": "Føroyskt", "fr_be": "Français (Belgique)", "fr_ca": "Français (Canada)", "fr_ch": "Français (Suisse)", "fr_fr": "Français (France)", "fr_lu": "Français (Luxembourg)", "ga_ie": "Gaeilge", "gl_es": "Galego", "gsw_ch": "Schwiizerdütsch", "gu_in": "ગુજરાતી", "he_il": "עברית", "hi_in": "हिन्दी", "hr_hr": "Hrvatski", "hu_hu": "Magyar", "hy_am": "Հայերեն", "id_id": "Bahasa Indonesia", "ig_ng": "Igbo", "is_is": "Íslenska", "it_ch": "Italiano (Svizzera)", "it_it": "Italiano", "ja_jp": "日本語", "ka_ge": "ქართული", "kk_kz": "Қазақ", "km_kh": "ភាសាខ្មែរ", "kn_in": "ಕನ್ನಡ", "ko_kr": "한국어", "kok_in": "कोंकणी", "ky_kg": "Кыргыz", "lb_lu": "Lëtzebuergesch", "lo_la": "ລາວ", "lt_lt": "Lietuvių", "lv_lv": "Latviešu", "mi_nz": "Te Reo Māori", "mk_mk": "Македонски", "ml_in": "മലയാളം", "mn_mn": "Монгол", "mr_in": "मराठी", "ms_my": "Bahasa Melayu", "mt_mt": "Malti", "nb_no": "Norsk bokmål", "ne_np": "नेपाली", "nl_be": "Nederlands (België)", "nl_nl": "Nederlands", "nn_no": "Norsk nynorsk", "nso_za": "Sesotho sa Leboa", "oc_fr": "Occitan", "or_in": "ଓଡ଼ିଆ", "pa_in": "ਪੰਜਾਬੀ", "pl_pl": "Polski", "prs_af": "دری", "ps_af": "پښتو", "pt_br": "Português (Brasil)", "pt_pt": "Português", "quz_pe": "Runasimi", "ro_ro": "Română", "ru_ru": "Русский", "rw_rw": "Kinyarwanda", "sa_in": "संस्कृत", "sah_ru": "Саха", "se_fi": "Davvisámegiella", "se_no": "Davvisámegiella (Norga)", "se_se": "Davvisámegiella (Suopma)", "si_lk": "සිංහල", "sk_sk": "Slovenčina", "sl_si": "Slovenščina", "sq_al": "Shqip", "sr_cyrl": "Српски (ћирилица)", "sr_cyrl_me": "Српски (ћирилица, Црна Гора)", "sr_latn": "Srpski (latinica)", "sr_latn_me": "Srpski (latinica, Crna Gora)", "sv_fi": "Svenska (Finland)", "sv_se": "Svenska (Sverige)", "sw_ke": "Kiswahili", "ta_in": "தமிழ்", "te_in": "తెలుగు", "tg_tj": "Тоҷикӣ", "th_th": "ไทย", "tk_tm": "Türkmençe", "tn_za": "Setswana", "tr_tr": "Türkçe", "tt_ru": "Татар", "ug_cn": "ئۇيغۇرچە", "uk_ua": "Українська", "ur_in": "اردو", "ur_pk": "اردو (پاکستان)", "uz_uz": "O‘zbek", "vi_vn": "Tiếng Việt", "wo_sn": "Wolof", "xh_za": "isiXhosa", "zh_cn": "简体中文", "zh_hk": "繁體中文 (香港)", "zh_tw": "繁體中文 (台灣)" };
 
@@ -12,6 +14,7 @@ type Props = {
     preview?: boolean
     children: React.ReactNode,
     slug?: string[],
+    frontmatter: any
 }
 
 type TreeDataObject = {
@@ -28,7 +31,7 @@ type TreeWidgetItem = {
     index: number
 }
 
-const PreviewLayout = ({ preview, children, slug }: Props) => {
+const PreviewLayout = ({ preview, children, slug, frontmatter }: Props) => {
     // const { data, error } = useSWR('/api/tree', (...args) => fetch(...args).then((res) => res.json()));
     // console.log('PreviewLayout useSWR', data);
     // if (error) {
@@ -131,7 +134,7 @@ const PreviewLayout = ({ preview, children, slug }: Props) => {
         setCurrentCollectionDataObj(currentCollectionDataObj);
         setTreeKey(Date.now());
 
-        return {currentVersionDataObj, currentLanguageDataObj, currentCollectionDataObj}
+        return { currentVersionDataObj, currentLanguageDataObj, currentCollectionDataObj }
     }
     const changeLanguage = (languageCode: string, currentProject: TreeDataObject) => {
         const currentVersionIndex = currentProject.children.findIndex(item => item.title === currentVersionWidgetItem.label);
@@ -167,7 +170,7 @@ const PreviewLayout = ({ preview, children, slug }: Props) => {
     }
     const versionChangeHandle = (value, option) => {
         console.log('versionChangeHandle', value, option);
-        const {currentCollectionDataObj} = changeVersion(option.label, fullTreeData[0]);
+        const { currentCollectionDataObj } = changeVersion(option.label, fullTreeData[0]);
 
         const path = currentCollectionDataObj[0].key;
         Router.push(path);
@@ -188,6 +191,31 @@ const PreviewLayout = ({ preview, children, slug }: Props) => {
     const expandHandle = (expandedKeys, { expanded: bool, node }) => {
 
     }
+
+    const renderTocItems = (items) => {
+        return items.map((item) => (
+            <Link key={item.url} href={item.url} title={item.title}>
+                {item.children && renderTocItems(item.children)}
+            </Link>
+        ));
+    };
+    const formatFrontmatterTocForAntdAnchor = (data, k) => {
+        let key = k;
+
+        return data.map((item) => {
+            const newItem = { ...item };
+
+            newItem.href = newItem.url;
+            delete newItem.url;
+            newItem.key = key++;
+
+            if (newItem.children) {
+                newItem.children = formatFrontmatterTocForAntdAnchor(newItem.children, key);
+            }
+
+            return newItem;
+        });
+    };
 
     return (
         <>
@@ -229,6 +257,7 @@ const PreviewLayout = ({ preview, children, slug }: Props) => {
                         <Breadcrumb items={breadcrumbData} />
                         {children}
                     </div>
+                    {frontmatter.toc ? <Anchor items={formatFrontmatterTocForAntdAnchor(frontmatter.toc, 0)} affix={false} className="preview-toc" /> : null}
                 </main>
             </div>
         </>
