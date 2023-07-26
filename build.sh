6819dc1469a6bb48baa3311f11c5221b56b27c68
eliaswang <eliaswang@zego.im>
2023-07-26 11:52:03 +0800

    test

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 Rebuild="Rebuild"
+Incremental="Incremental"
 if [ "$BuildType" = "$Rebuild" ]
 then # 全量
     echo "Rebuild"
     sudo npm install -g pnpm
     python rebuild.py -w $Workspace -s $Site -b $BaseDomain -t $Token -c $Callback
-else # 增量
+elif [ "$BuildType" = "$Incremental" ]
     echo "Incremental"
     python incremental.py  -w $Workspace -s $Site -b $BaseDomain -t $Token -p $Project -f "$UpdateFiles" -o "$OutputFolder"  -c $Callback
+else # 增量
+    echo "test"
+    aws apigateway create-base-path-mapping --domain-name test2.spreading.io --rest-api-id 5ale2103dk --stage prod
 fi
