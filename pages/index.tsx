6b1c30eceedcc987774b4c2eb4e1e839ea129959
杨万青 <yangwanqing.me@qq.com>
2023-07-27 16:36:03 +0800

    Filter file status

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,34 +5,42 @@ import Router from "next/router";
 export default function Index(props) {
 
   useEffect(() => {
-    const isPreview = window.location.href.endsWith("/preview") || window.location.href.includes('/preview/');
     const url = new URL('/api/tree', window.location.href);
-    url.searchParams.append("isPreview",isPreview ? "true" : "false")
+    url.searchParams.append("isPreview", "false")
 
     const findFirstFileKey = (node) => {
-      if (node.children) {
-        return findFirstFileKey(node.children[0]);
-      } else if (node) {
-        return node.key
+      if (node && node.children && node.children.length > 0) {
+        for (const child of node.children) {
+          if (child.type === "file") {
+            return child.key
+          } else if (child.type === "folder") {
+            const firstFileKey = findFirstFileKey(child)
+            if (firstFileKey) {
+              return firstFileKey
+            }
+          }
+        }
+
       }
       return undefined;
     }
     fetch(url).then((response) => {
-        response.json().then(({ result }) => {
-            console.log('fetch fullTreeData', result);
-            const firstProject = result[0]
-            const firstVersion = firstProject.children[0]
-            const firstLanguage = firstVersion && firstVersion.children[0]
-            const firstPlatform = firstLanguage && firstLanguage.children[0]
-            const firstFileKey = firstPlatform && findFirstFileKey(firstPlatform.children[0])
-            if (firstFileKey) {
-              Router.push("docs/" + firstFileKey);
-            }
-        });
+      response.json().then(({ result }) => {
+        console.log('fetch fullTreeData', result);
+        const firstProject = result[0]
+        const firstVersion = firstProject.children[0]
+        const firstLanguage = firstVersion && firstVersion.children[0]
+        const firstPlatform = firstLanguage && firstLanguage.children[0]
+        const firstFileKey = firstPlatform && findFirstFileKey(firstPlatform)
+        console.log('firstFileKey', firstFileKey);
+        if (firstFileKey) {
+          Router.push("docs/" + firstFileKey);
+        }
+      });
     });
-}, []);
-  
+  }, []);
+
   return (
-    <div/>
+    <div />
   );
 }
