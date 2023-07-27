471bb6f455e1da50147e5ea82f3c0f71930430eb
eliaswang <eliaswang@zego.im>
2023-07-27 11:30:22 +0800

    .

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ then
     python incremental.py  -w $Workspace -s $Site -b $BaseDomain -t $Token -p $Project -f "$UpdateFiles" -o "$OutputFolder"  -c $Callback
 else # 增量
     echo "test"
-    aws apigateway create-domain-name --domain-name test3.spreading.io --certificate-arn arn:aws:acm:ap-east-1:547537176727:certificate/07cd13ee-39c7-43a1-a2b8-5881fd0c3e96 --region ap-southeast-1
+    aws apigateway create-domain-name --domain-name test3.spreading.io --certificate-arn arn:aws:acm:us-east-1:547537176727:certificate/06e7f446-2952-42ad-bb0c-9d8e5e144808 --region ap-southeast-1
     aws apigateway create-base-path-mapping --domain-name test3.spreading.io --base-path "" --rest-api-id 5ale2103dk --region ap-southeast-1
 fi
