60e6ba0037c43816db4849fd655e2d337600c50f
elias <eliaswang@zego.im>
2023-08-11 15:59:49 +0800

    test

--- rebuild.py
+++ rebuild.py
@@ -147,24 +147,24 @@ if __name__ == '__main__':
             sys.exit(2)
 
         # 制品目录，先清理下历史制品
-        products_dir = workspace+"_products/"+site
-        subprocess.call(["aws", "s3", "rm", "s3://zego-spreading-test/"+products_dir, "--recursive"])
+        # products_dir = workspace+"_products/"+site
+        # subprocess.call(["aws", "s3", "rm", "s3://zego-spreading-test/"+products_dir, "--recursive"])
 
-        #cpoutput = subprocess.check_output(["cp", "-rf", ".aws-sam/build/NextFunction", "/mnt/spreading-15-44"])
-        #print("cp:", cpoutput.decode("utf-8"))
-        # deploy
-        stack = workspace.replace("_", "-")+"-"+site
-        code = subprocess.call(["sam", "deploy", "--stack-name", stack, "--s3-bucket", "zego-spreading-test", "--s3-prefix", products_dir])
-        if code != 0:
-            report_build_status(callback_url, 500, "sam deploy error", i_ws, site, "")
-            sys.exit(2)
+        # #cpoutput = subprocess.check_output(["cp", "-rf", ".aws-sam/build/NextFunction", "/mnt/spreading-15-44"])
+        # #print("cp:", cpoutput.decode("utf-8"))
+        # # deploy
+        # stack = workspace.replace("_", "-")+"-"+site
+        # code = subprocess.call(["sam", "deploy", "--stack-name", stack, "--s3-bucket", "zego-spreading-test", "--s3-prefix", products_dir])
+        # if code != 0:
+        #     report_build_status(callback_url, 500, "sam deploy error", i_ws, site, "")
+        #     sys.exit(2)
 
-        # 读取api id
-        api_id = subprocess.check_output(["aws", "cloudformation", "describe-stacks", "--stack-name", stack, "--query", "Stacks[0].Outputs[?OutputKey==`ApiId`].OutputValue", "--output", "text"])
-        api_id = api_id.decode("utf-8").rstrip("\n\t\r")
-        print("api_id:", api_id)
+        # # 读取api id
+        # api_id = subprocess.check_output(["aws", "cloudformation", "describe-stacks", "--stack-name", stack, "--query", "Stacks[0].Outputs[?OutputKey==`ApiId`].OutputValue", "--output", "text"])
+        # api_id = api_id.decode("utf-8").rstrip("\n\t\r")
+        # print("api_id:", api_id)
 
-        report_build_status(callback_url, 0, "success", i_ws, site, api_id)
+        # report_build_status(callback_url, 0, "success", i_ws, site, api_id)
     except Exception as e:
         report_build_status(callback_url, 500, str(e), i_ws, site, "")
         sys.exit(2)
--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 [ ! -d '/tmp/cache' ] && mkdir -p /tmp/cache
 
-ll /mnt/spreading-15-44
+ls -l /mnt/spreading-15-44
 cd /mnt/spreading-15-44
 exec node server.js
