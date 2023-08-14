6e4bb3521fc5338cc2c4bee4160488dfa896d798
elias <eliaswang@zego.im>
2023-08-14 16:17:21 +0800

    run mount test

--- run.sh
+++ run.sh
@@ -2,6 +2,18 @@
 
 [ ! -d '/tmp/cache' ] && mkdir -p /tmp/cache
 
-ls -l /mnt/spreading-15-44
-cd /mnt/spreading-15-44
+ls -l /mnt/spreading-15-89
+cd /mnt/spreading-15-89
+
+mkdir -p .next/pages/docs
+mkdir -p _docs
+
+sudo mount -t efs -o tls,accesspoint=fsap-0048da4b9e1e50d72 fs-0d941cb6e4d5d582e:/ .next/pages/docs
+sudo mount -t efs -o tls,accesspoint=fsap-007645e88f391dc7a fs-0d941cb6e4d5d582e:/ _docs
+
+touch .next/pages/docs/page.text
+touch _docs/docs.text
+
+df -h
+
 exec node server.js
